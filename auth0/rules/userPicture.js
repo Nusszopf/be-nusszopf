100704b5d60af9338ff70989c0d92e89da6f3839
schwamic <ms@tresmo.de>
2021-02-05 17:20:30 +0100

    refactor(auth0): update rules

--- auth0/rules/userPicture.js
+++ auth0/rules/userPicture.js
@@ -4,6 +4,9 @@ function userPicture(user, context, cb) {
   if (!isSocialAccount) {
     cb(null, user, context);
   } else {
+    const isTestUser = user.user_metadata && user.user_metadata.isTestUser === "true";
+    const hasuraUrl = isTestUser ? "https://nz-hasura-stage.herokuapp.com/v1/graphql" : "https://nz-hasura-prod.herokuapp.com/v1/graphql";
+    const hasuraSecret = isTestUser ? "<hasura-stage-secret>" : "<hasura-prod-secret>"; // todo: replace '<hasura-<env>-secret>'
     const query = `
 		mutation($userId:String!,$userPicture:String!){
       update_users_by_pk(pk_columns:{id:$userId},_set:{picture:$userPicture}){id}
@@ -12,11 +15,11 @@ function userPicture(user, context, cb) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "x-hasura-admin-secret": "my-admin-secret", // todo: replace 'my-admin-secret'
+        "x-hasura-admin-secret": hasuraSecret,
       },
       body: JSON.stringify({ query, variables: { userId: user.user_id, userPicture: user.picture } }),
     };
-    fetch("https://nz-hasura-prod.herokuapp.com/v1/graphql", options)
+    fetch(hasuraUrl, options)
       .then(() => {
         cb(null, user, context);
       })
